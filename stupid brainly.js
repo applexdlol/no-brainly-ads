2438fa1f8ec1b472b12d0714b1cc94b03555a2d1
applexdlol <88859575+applexdlol@users.noreply.github.com>
2023-09-08 20:38:16 -0700

    Create stupid brainly.js

---
+++ stupid brainly.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         BRAINLY HACK
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://brainly.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=brainly.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    localStorage.setItem("flexible-funnel-previews", "");
+})();
